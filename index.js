964b4148b9999c4b38982cab69829cf5ac833ae1
sachetwasti1997 <wasti_sachet@optum.com>
2023-04-27 07:47:19 +0530

    Code Push

---
+++ index.js
@@ -0,0 +1,44 @@
+const { log } = require('console');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+const post = {};
+
+app.get('/posts', (req, res) => {
+    res.send(post);
+});
+
+app.post('/posts', async(req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    
+    post[id] = {
+        id, title
+    };
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'POST',
+        data: {
+            id, title
+        }
+    });
+
+    res.status(201).send(post[id])
+});
+
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
+});
+
+app.listen(4000, () => {
+    log('Listening on port: 4000')
+});
